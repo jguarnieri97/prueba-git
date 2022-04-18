b6b8db57e08656622e862f062bf27bb14878df01
jguarnieri <jguarnieri@nec.com.ar>
2022-04-18 20:21:41 -0300

    Clase calculador

--- src/ar/edu/unlam/pb2/pruebagit/PruebaGit.java
+++ src/ar/edu/unlam/pb2/pruebagit/PruebaGit.java
@@ -1,10 +1,17 @@
 package ar.edu.unlam.pb2.pruebagit;
 
+import ar.edu.unlam.pb2.pruebagit.calculador.Calculador;
+
 public class PruebaGit {
+	
+	
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		
+		Calculador calculador = new Calculador(10, 15);
+		
+		System.out.println("El resultado es: " + calculador.getResult());
 	}
 
 }
---
+++ src/ar/edu/unlam/pb2/pruebagit/calculador/Calculador.java
@@ -0,0 +1,17 @@
+package ar.edu.unlam.pb2.pruebagit.calculador;
+
+public class Calculador {
+	
+	private Integer result;
+	
+	public Calculador(Integer v1, Integer v2) {
+		this.result = v1 + v2;
+	}
+
+	public Integer getResult() {
+		return result;
+	}
+	
+	
+
+}
